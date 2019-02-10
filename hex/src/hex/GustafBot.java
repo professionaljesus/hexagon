de53945afc990cd64ac9080fe142a69e638595b4
gusta <gusta@192.168.0.103>
2019-02-10 14:55:36 +0100

    minbo

---
+++ hex/src/hex/GustafBot.java
@@ -0,0 +1,17 @@
+package hex;
+
+public class GustafBot extends Player{
+
+	public GustafBot(int id, int size) {
+		super(id, size);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void algo(HexaMap H) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
