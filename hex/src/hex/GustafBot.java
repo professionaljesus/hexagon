86aeb9a3a6da435ab39a70110afec4a154517073
gusta <gusta@192.168.0.103>
2019-02-10 15:09:39 +0100

    g

--- hex/src/hex/GustafBot.java
+++ hex/src/hex/GustafBot.java
@@ -14,9 +14,22 @@ public class GustafBot extends Player{
 	@Override
 	public int[] algo(HexaMap H) {
 		int[] moves = new int[5];
-
+		
 		this.getMap(H);
 		
+		Hexagon[][] m = this.myMap;
+		
+		
+		for(int x = 0; x < m.length; x++) {
+			for(int y = 0; y < m[x].length; y++) {
+				if(m[x][y] != null) {
+					if(m[x][y].getOwner() == this.id)
+						H.GetNeighbour(x, y, 1);
+				}
+			}
+		}
+			
+
 		
 		return moves;
 		
