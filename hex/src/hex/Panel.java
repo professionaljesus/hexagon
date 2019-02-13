0393075b7fb42bf98b20194b90f5d00115ce6f01
da7308ca-s <da7308ca-s@student.lu.se>
2019-02-13 19:41:12 +0100

    asd

--- hex/src/hex/Panel.java
+++ hex/src/hex/Panel.java
@@ -44,7 +44,7 @@ public class Panel extends JPanel implements Runnable, KeyListener{
 
 
 		player[0] = new BeppeBot(1,mapsize, Color.GREEN, "BEPPNATION");
-		player[1] = new BeppeBot(2,mapsize, Color.BLUE, "WILDCARD");
+		player[1] = new SimpleBot(2,mapsize, Color.BLUE, "WILDCARD");
 		player[2] = new GustafBot2(3,mapsize, Color.RED, "GURRA");
 
 		H = new HexaMap(mapsize,width,height,player);
