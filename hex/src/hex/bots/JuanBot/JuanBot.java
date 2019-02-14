d6d70d64f814402ed464cd80f893da35d1475b68
JohanKJIP <JohanKJIP@gmail.com>
2019-02-14 21:02:20 +0100

    better hex selection

--- hex/src/hex/bots/JuanBot/JuanBot.java
+++ hex/src/hex/bots/JuanBot/JuanBot.java
@@ -4,7 +4,6 @@ import hex.Hexagon;
 import hex.Player;
 
 import java.awt.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class JuanBot extends Player {
@@ -21,32 +20,59 @@ public class JuanBot extends Player {
 
     @Override
     public int[] algo(HashSet<Hexagon> H) {
+        ArrayList<Hexagon> hAsList = new ArrayList<>(H);
         // Categorise hexes
-        ArrayList<HexPair> notOwnedHexPairs = getHexPairs(H, HexType.NOTOWNED);
-        ArrayList<HexPair> ownedHexPairs = getHexPairs(H, HexType.OWNED);
+        ArrayList<HexPair> notOwnedHexPairs = getHexPairs(new ArrayList<>(hAsList), HexType.NOTOWNED);
+        ArrayList<HexPair> ownedHexPairs = getHexPairs(new ArrayList<>(hAsList), HexType.OWNED);
 
-        HexPair selectedHexPair = notOwnedHexPairs.get(rand.nextInt(notOwnedHexPairs.size()));
-        Hexagon parent = selectedHexPair.getParent();
-        Hexagon target = selectRandomHex(selectedHexPair.getChildren());
+        int[] instruction = null;
+        // If we can expand without competition, DO IT!
+        if (notOwnedHexPairs.size()>0) {
+            HexPair selectedHexPair = notOwnedHexPairs.get(rand.nextInt(notOwnedHexPairs.size()));
+            Hexagon parent = selectedHexPair.getParent();
+            Hexagon target = selectCloseHex(selectedHexPair.getChildren());
 
-        int[] instruction = new int[6];
-        instruction[0] = id;
-        instruction[1] = 1;
-        instruction[2] = parent.getX();
-        instruction[3] = parent.getY();
-        instruction[4] = target.getX();
-        instruction[5] = target.getY();
-        System.out.println(Arrays.toString(instruction));
+            instruction = createInstruction(1, parent, target);
+        }
         return instruction;
     }
 
     /**
+     * Method to select non-owned hexagon with the most owned neighbours.
+     * @param hexagons list of hexagons.
+     * @return hexagon with most owned neighbours.
+     */
+    private Hexagon selectCloseHex(ArrayList<Hexagon> hexagons) {
+        Hexagon lowest = null;
+        int highestNeighbourCount = 0; // can not be more than 6?
+        for (Hexagon hex: hexagons) {
+            int ownedNeighbours = getHexPairs(new ArrayList<>(Arrays.asList(hex.getNeighbours())), HexType.OWNED).size();
+            if (ownedNeighbours > highestNeighbourCount) {
+                highestNeighbourCount = ownedNeighbours;
+                lowest = hex;
+            }
+        }
+        return lowest;
+    }
+
+    /**
+     * Method to create instruction from res, parent and target.
+     * @param res resource to move.
+     * @param parent, parent hexagon.
+     * @param target, target hexagon.
+     * @return instruction for game.
+     */
+    private int[] createInstruction(int res, Hexagon parent, Hexagon target) {
+        return new int[]{id, res, parent.getX(), parent.getY(), target.getX(), target.getY()};
+    }
+
+    /**
      * Get hex pairs depending on HexType.
-     * @param hexMap
-     * @param type
+     * @param hexMap, map of game.
+     * @param type, type of hexes to return.
      * @return parent and neighbours of HexType type.
      */
-    private ArrayList<HexPair> getHexPairs(HashSet<Hexagon> hexMap, HexType type) {
+    private ArrayList<HexPair> getHexPairs(ArrayList<Hexagon> hexMap, HexType type) {
         ArrayList<HexPair> hpList = new ArrayList<>();
         for (Hexagon hex: hexMap) {
             Hexagon[] neighbours = hex.getNeighbours();
@@ -96,11 +122,8 @@ public class JuanBot extends Player {
      * @return random hex.
      */
     private Hexagon selectRandomHex(ArrayList<Hexagon> hexMap) {
-        if (hexMap.size() > 0) {
-            int selection = rand.nextInt(hexMap.size());
-            return hexMap.get(selection);
-        }
-        return null;
+        int selection = rand.nextInt(hexMap.size());
+        return hexMap.get(selection);
     }
 
     /**
